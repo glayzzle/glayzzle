04d482c8951f997ea33f63df9eaf51d2c01e8d80
Ioan CHIRIAC <i.chiriac@yahoo.com>
2014-04-03 04:41:09 +0200

    add some tests

--- test/main.js
+++ test/main.js
@@ -1,3 +1,6 @@
 var PHP = require('../src/php');
 PHP.include('./foo.php');
-console.log('Foo Result : ' + PHP.globals.foo("Hello World"));
+console.log('Foo Result : ' + PHP.foo("Hello World"));
+
+var bar = new PHP.bar('azerty');
+console.log('Bar result : ' + bar.foo);
