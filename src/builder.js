2729115081a289cff58f924b79d9f87febdda5f2
Ioan CHIRIAC <i.chiriac@yahoo.com>
2014-07-27 12:29:03 +0200

    remove pegjs builder

--- src/builder.js
+++ src/builder.js
@@ -10,94 +10,20 @@ var path = require('path');
 
 module.exports = {
   /** MODIFIERS CONSTANTS **/
-  T_PUBLIC:     1,
-  T_PROTECTED:  2,
-  T_PRIVATE:    4,
-  T_STATIC:     8,
-  T_ABSTRACT:   16,
-  T_FINAL:      32,
+   T_PUBLIC:     1
+  ,T_PROTECTED:  2
+  ,T_PRIVATE:    4
+  ,T_STATIC:     8
+  ,T_ABSTRACT:   16
+  ,T_FINAL:      32
   
-  compat: null,
-  requires: [],
-  functions: [],
-  classes: {},
-  serialize: {},
-  filename: null,
-  directory: null,
-  // compiles again the parser using pegjs
-  compile: function(cb, cb_err) {
-    var PEG = require('pegjs');
-    var fs = require('fs');
-    var PHP = require('./php');
-    var GRAMMAR_PATH = __dirname + '/grammar/';
-    if (process.env.DEBUG > 0) console.log('> Read parsing rules');
-    // Reads a list of files and executes the specified callback
-    var readFiles = function(files, cb) {
-      var counter = files.length;
-      var result = { files: {}, errors: []};
-      if (!files || files.length == 0) {
-        cb(result);
-      } else {
-        files.forEach(function (file){
-          fs.readFile(file, 'utf8', function(err,data) {
-            if (err) {
-              result.errors.push(err);
-            } else {
-              result.files[file] = data;
-            }
-            counter --;
-            if (counter == 0) cb(result);
-          });
-        });
-      }
-    };
-    fs.readFile(GRAMMAR_PATH + 'php.pegjs', 'utf8', function(err, data) {
-      if (err) {
-        if (cbErr) cbErr(err);
-        return;
-      }
-      if (process.env.DEBUG > 0) console.log('> Build parser');
-      var importRegex = /^@import\s+'([A-Za-z0-9\-_.]*)'$/mg;
-      var files = [];
-      match = importRegex.exec(data);
-      while (match != null) {
-        files.push(match[1]);
-        match = importRegex.exec(data);
-      }
-      if (files && files.length > 0) {
-        for(var i = 0; i < files.length; i++) {
-          files[i] = GRAMMAR_PATH + files[i];
-        }
-      } else {
-        files = [];
-      }
-      readFiles(files, function(imports) {
-        try {
-          data = data.replace(
-            importRegex,
-            function(match, file) {
-              return imports.files[GRAMMAR_PATH + file];
-            }
-          );
-          fs.writeFileSync(
-            __dirname + '/parser.js'
-            , 'module.exports = ' 
-            + PEG.buildParser(data, {
-              cache:    false,
-              output:   'source',
-              optimize: 'speed',
-              plugins:  []
-            })
-            + ';\n'
-          );
-        } catch(e) {
-          PHP.context.parseError(e, data);
-        }
-        if (process.env.DEBUG > 0) console.log('*** BUILD DONE ***');
-        if (cb) cb();
-      });
-    });
-  }
+  ,compat:      null
+  ,requires:    []
+  ,functions:   []
+  ,classes:     {}
+  ,serialize:   {}
+  ,filename:    null
+  ,directory:   null
   // initialize the specified file
   ,init: function(filename) {
     this.filename = filename;
