84ac20e55df90f9dea6c7bf2ffe973980b2f6389
Ioan CHIRIAC <i.chiriac@yahoo.com>
2015-08-03 02:01:42 +0200

    ...

--- src/context.js
+++ src/context.js
@@ -1,280 +1,183 @@
 /**
  * Glayzzle : PHP on NodeJS
  * @url http://glayzzle.com
- * @author Ioan CHIRIAC
  * @license BSD-3-Clause
  */
 
 var fs = require('fs');
-var os = require('os');
-var crypto = require('crypto');
+var md5 = require('crypto').createHash('md5');
 var builder = require('./builder');
-var path = require('path');
-var util = require('util');
 
-// Searching helper for a Buffer
-// From : https://groups.google.com/forum/#!topic/nodejs/fHiHwJZc6b4
-if ( !Buffer.prototype.indexOf ) {
-  Buffer.prototype.indexOf = function(str) {
-    if (typeof str !== 'string' || str.length === 0 || str.length > this.length) return -1;
-    var search = str.split("").map(function(el) { 
-      return el.charCodeAt(0); 
-    }), searchLen = search.length, ret = -1, i, j, len;
-    
-    for (i=0,len=this.length; i<len; ++i) {
-      if (this[i] == search[0] && (len-i) >= searchLen) {
-        if (searchLen > 1) {
-          for (j=1; j<searchLen; ++j) {
-            if (this[i+j] != search[j]) 
-              break;
-            else if (j == searchLen-1) {
-              ret = i;
-              break;
-            }
-          }
-        } else ret = i;
-        if (ret > -1) break;
-      }
+// Converts a filename to a cache filename 
+function getCacheFile(php, filename) {
+  return php.context.tmp 
+    + php.constant('DIRECTORY_SEPARATOR') 
+    + 'glz.' + this.php.VERSION 
+    + '-' + md5.update(filename).digest('hex') 
+    + '.js'
+  ;
+}
+
+/**
+ * The running context
+ */
+var context = function(php) {
+  this.php        = php;
+  this.builder    = new builder(php);
+  this.constants  = require('./constants')(php);
+  this.parser     = require('php-parser');
+};
+
+// define temporary path
+Object.defineProperty(context.prototype, "tmp", {
+  enumerable: false,
+  configurable: false,
+  writable: false,
+  value: require('os').tmpdir()
+});
+
+// list of included buffers
+Object.defineProperty(context.prototype, "includes", {
+  enumerable: true,
+  configurable: false,
+  writable: false,
+  value: {}
+});
+
+/**
+ * evaluates the specified code and returns its function
+ */
+context.prototype.eval = function(code) {
+
+
+  // 1. Convert PHP code to AST
+  try {
+    var ast = this.parser.parseEval(code);
+  } catch(e) {
+    // @todo parse error
+    throw e;
+  }
+
+  // 2. Convert AST to Javascript
+  try {
+    code = this.builder.getCode(ast, 'evald code');
+  } catch(e) {
+    // @todo parse error
+    throw e;
+  }
+
+  // 3. Evaluate the javascript
+  try {
+    code = Function('php', 
+      code.header.join(';\n')
+      + 'return {' + code.functions.join(',\n') + '};'
+    )(this.php);
+  } catch(e) {
+    // @todo parse error
+    throw e;
+  }
+
+  // 4. Execute the eval body
+  return code.__main();
+};
+
+/**
+ * includes the specified file
+ */
+context.prototype.include = function(filename) {
+  // 1. Resolves the filename
+  var path = this.php.path(filename);
+
+  // 2. Check memory cache
+  var once = this.includes.hasOwnProperty(path);
+  var now = Date.now();
+  // check memory status (ttl 2 sec)
+  if (once && now - this.includes[path].time < 2000) {
+    return this.includes[path].module.__main();
+  }
+
+  // 3. Check file cache
+  var cache = getCacheFile(this.php, path);
+  try {
+    var fileStat = fs.statSync(path).mtime;
+    var cacheStat = fs.statSync(cache).mtime;
+  } catch(e) {
+    if (!fileStat) {
+      throw Error('Unable to locate file ' + filename);
+    } else if (!cacheStat) {
+      this.writeAsCache(path, cache);
+      cacheStat = now;
+      once = true;
     }
-    return ret;
-  };
-}
-if ( !Buffer.prototype.substring ) {
-  Buffer.prototype.substring = function(start, end) {
-    return this.toString('utf8', start, end);
-  };
-}
-// This module handles the project 
-module.exports = {
-
-  // defines the default temporary path
-  tmp: os.tmpdir()
-
-  // list of included buffers
-  ,includes: {}
-
-  // list of reflection entries
-  ,relection: {
-    functions: {},
-    classes: {}
   }
-
-  // defines the configuration entry
-  ,conf: {
-    asp_tags: false,
-    short_tags: false
-  }
-
-  // PHP engine
-  ,php: null
-
-  // initialize the current context with specified PHP engine
-  ,init: function(php) {
-    this.php = php;
-    return this;
-  }
-
-  /**
-   * Shows a parsing error message and outputs the source at the specified line
-   */
-  ,parseError: function(e, file) {
-    if(e.line) {
-      util.error(
-        "\nParse Error : " + e.message + "\n"
-        + "At line " + e.line + ', ' + e.column
-      );
-      if ( file ) {
-        file = file.toString();
-        console.log("\nSource Code :\n");
-        var line = e.line - 2;
-        if (line < 0) line = 0;
-        var pstart = 0;
-        var pnext = 0;
-        for(var i = 0; i < line; i++) {
-          pnext = file.indexOf("\n", pstart);
-          if ( pnext < 0 ) {
-            pstart = 0;
-            break;
-          }
-          pstart = pnext + 1; 
-        }
-        var pend = pstart;
-        for(var i = 1; i < 5; i++) {
-          pnext = file.indexOf("\n", pend);
-          if ( pend > pnext ) {
-            pnext = file.length;
-            break;
-          }
-          var curLine = line + i;
-          var loc = file.substring(pend, pnext);
-          if (curLine ==  e.line ) {
-            loc = '>> ' + loc;
-          }
-          if (loc.length > 74) {
-            loc = loc.substring(0, 71) + '...';
-          }
-          console.log( curLine + '. ' + loc );
-          pend = pnext + 1;
-        }
-        if (process.env.DEBUG > 0) {
-          if (!e.stack) {
-            e.stack = (new Error(e.message)).stack;
-          }
-          console.log('\n' + e.stack);
-        }
-        process.exit(1);
+  
+  // 4. Run the cache if needed
+  if ( cacheStat > fileStat ) {
+    if (!once) {
+      // the cache is ok but never included yet
+      this.includes[path] = {
+        time: now,
+        module: require(path)(this.php)
       }
     } else {
-      util.error(e.stack || e);
+      this.includes[path].time = now;
     }
-    return false;
+  } else {
+    this.writeAsCache(path, cache);
   }
-  // Converts a filename to a cache filename 
-  ,getCacheFile: function(filename) {
-    var result = this.tmp + path.sep + 'glz.' + this.php.VERSION + '-' + crypto.createHash('md5').update(filename).digest('hex') + '.js';
-    return result;
-  }
-  // (parse if not cached or updated) and returns the specified file structure
-  ,get: function(filename) {
-    // check memory cache
-    var once = this.includes.hasOwnProperty(filename);
-    var now = Date.now();
-    // check memory status
-    if (once && now - this.includes[filename].time < 2000) {
-      return this.includes[filename].module;
-    }
-    // check files cache
-    var cache = this.getCacheFile(filename);
-    try {
-      var cacheStat = fs.statSync(cache); 
-      var filenameStat = fs.statSync(filename);
-      if ( cacheStat.mtime > filenameStat.mtime ) {
-        if (!once) {
-          // include the cache file for the first time
-          return this.register(filename, cache, !once);
-        }
-        this.includes[filename].time = now;
-        return this.includes[filename].module;
-      }
-    } catch(e) {
-      // original file not found ?
-      if (cacheStat && !filenameStat) return false;
-    }
-    // parse the php file
-    return this.refresh(filename, cache, !once);
-  }
-  // registers the specified file
-  , register: function(filename, cache, refresh) {
-    delete require.cache[cache];
-    var include = this.includes[filename] = {
-      module: require(cache)
-      , time: Date.now()
-    };
-    // registers each function globally
-    for(var name in include.module) {
-      if (!refresh && this.relection.functions.hasOwnProperty(name)) {
-        throw new Error(
-          'Can not re-declare function "'+name
-          +'", already defined in '
-          +this.relection.functions[name].filename+' !'
-        )
-      }
-      this.php.globals[name] = include.module[name];
-      this.relection.functions[name] = {
-        filename: filename
-      };
-    }
-    return include.module;
-  }
-  // evaluates the specified code and returns its function
-  , eval: function(code) {
-    var parser = builder.getParser();
-    parser.lexer.all_tokens = false;
-    parser.lexer.mode_eval = true;
-    var AST = parser.parse(code);
-    console.log(
-      util.inspect(
-        AST, { 
-          showHidden: false, 
-          depth: 10, 
-          colors: true 
-        }
-      )
+  
+  // 5. Executes the code
+  return this.includes[path].module.__main();
+};
+
+// reads the specified file and saves it to cache
+context.prototype.writeAsCache = function(filename, cache) {
+  // 1. Convert PHP code to AST
+  try {
+    var ast = this.parser.parse(
+      fs.readFileSync(filename), filename
     );
-    /** tokens ** /
-    var jsTok = this.php.globals.__call( 'token_get_all', '<?php ' + code );
-    for(var i = 0; i < jsTok.length; i++) {
-      if (jsTok[i] instanceof Array) {
-        jsTok[i][0] = this.php.globals.__call(
-          'token_name', jsTok[i][0]
-        );
-      }
-    }
-    console.log(
-      util.inspect(
-        jsTok, { 
-          showHidden: false, 
-          depth: 10, 
-          colors: true 
-        }
-      )
-    ); /** **/
-    return { __main: function() { return true; } };
-    process.exit(0);
-    builder.init('evald code');
-    builder.functions.push(builder.getMainFunction(AST));
-    var source = builder.headers() + '\n'
-      + 'exec = {\n\t'
-        + '__output: process.stdout,\n\t'
-        + builder.functions.join('\n\t,')
-      + '};'
-    ;
-    var exec = null;
-    if (process.env.DEBUG > 9) console.log(source);
-    eval(source);
-    // registers each function globally
-    for(var name in exec) {
-      if (this.relection.functions.hasOwnProperty(name)) {
-        throw new Error(
-          'Can not re-declare function "'+name
-          +'", already defined in '
-          +this.relection.functions[name].filename+' !'
-        )
-      }
-      this.php.globals[name] = exec[name];
-    }
-    return exec;
+  } catch(e) {
+    // @todo parse error
+    throw e;
   }
-  // parse and build file cache
-  , refresh: function(filename, cache, refresh) {
-    var parser = builder.getParser();
-    parser.lexer.all_tokens = false;
-    parser.lexer.mode_eval = false;
-    var data = fs.readFileSync(filename);
-    if (process.env.DEBUG > 0) console.log("-> Parse " + filename);
-    try {
-      var results = parser.parse(data);
-      builder.init(filename);
-      builder.functions.push(builder.getMainFunction(results));
-      source = '/** GLAYZZLE GENERATED CODE : '+filename+' ('+cache+') **/\n\n' 
-        + builder.headers() + '\n'
-        + 'module.exports = {\n\t'
-          + '__output: process.stdout,\n\t'
-          + builder.functions.join('\n\t,')
-        + '};'
-      ;
-      if (process.env.DEBUG > 0) {
-        console.log('** AST :');
-        console.log(util.inspect(results, { depth: process.env.DEBUG, colors: true}));
-        console.log(source);
-        console.log('** RUNTIME OUTPUT :');
-      }
-      fs.writeFileSync(cache, source, {
-        flag: 'w+'
-      });
-      return this.register(filename, cache, refresh);
-    } catch(e) {
-      return this.parseError(e, data);
-    }
+
+  // 2. Convert AST to Javascript
+  try {
+    var code = this.builder.getCode(ast, filename);
+  } catch(e) {
+    // @todo parse error
+    throw e;
+  }
+
+  // 3. Save the code to cache
+  delete require.cache[cache];
+  try {
+    fs.writeFileSync(
+      cache
+      , '/** GLAYZZLE GENERATED CODE : '+filename+' ('+cache+') **/\n\n' 
+      + 'module.exports = function(php) {\n\t'
+      + code.header.join(';\n\t')
+      + '\n\treturn {' + code.functions.join(',\n\t\t') + '\n\t};'
+      + '\n};' 
+      , { flag: 'w+' }
+    );
+  } catch(e) {
+    // @todo system write error
+    throw e;
+  }
+  
+  // 4. Include it
+  try {
+    this.includes[filename] = {
+      time: Date.now(),
+      module: require(cache)(this.php)
+    };
+  } catch(e) {
+    // @todo parse error
+    throw e;
   }
 };
+
+// exporting the context class
+module.exports = context;
--- src/php.js
+++ src/php.js
@@ -1,119 +1,193 @@
 /**
- * Glayzzle : PHP on NodeJS
+ * Glayzzle - run PHP on NodeJS
  * @url http://glayzzle.com
- * @author Ioan CHIRIAC
  * @license BSD-3-Clause
  */
 
 var util = require('util');
-var path = require('path');
 var fs = require('fs');
+var pkg = require('../package.json');
+var context = require('./context');
+
 
 /**
- * PHP helper (makes NodeJS and PHP to communicate)
+ * php main class
  * 
  * Usage : 
  * 
  * demo.js :
- * var PHP = require('./src/php');
- * var result = PHP.include('./demo.php');
+ * var glayzzle = require('./src/php');
+ * var php = new glayzzle();
+ * var result = php.include('./demo.php');
  * console.log(result);
- * console.log(PHP.globals.foo(5));
+ * console.log(php.foo(5));
  * 
  * demo.php
  * function foo($x) {
  *   return $x;
  * }
  * return "foo";
- * 
  */
-module.exports = {
-  // Current version
-  VERSION: '0.0.6',
-
-  // contains the PHP tokenizer
-  _parser: false,
-
-  // retro-PHP-compatibility layer
-  globals: false,
-
-  // current execution context
-  context: false,
-
-  // gets the parser
-  parser: function() {
-    if (!this._parser) {
-      this._parser = require('./parser').parser;
-    }
-    return this._parser;
-  },
-
-  // cleans actual context
-  clean: function() {
-    this.globals = require('./compat');
-    this.context = require('./context').init(this);
-    this._parser = false;
-    return this;
+var php = function(config) {
+  this.config = config || {};
+  // initialize the context
+  this.context = new context(this);
+  // sets the temporary directory
+  if (this.config.hasOwnProperty('sys_temp_dir')) {
+    this.context.tmp = this.config.sys_temp_dir;
   }
-  // run a eval over PHP code
-  ,eval: function(code, ignore, output) {
-    try {
-      return this.context.eval(code).__main(
-        output ? output : process.stdout
-      );
-    } catch(e) {
-      if (!ignore) throw e;
-    }
-  }
-  /**
-   * Includes a PHP script
-   */
-  ,include: function(filename, ignore, output, once) {
-    filename = path.resolve(filename);
-    if (once && this.context.includes.hasOwnProperty(filename)) {
-      return;
-    }
-    try {
-      var code = this.context.get(filename);
-    } catch(e) {
-      if (!ignore) {
-        return util.error(
-          'Warning : T_INCLUDE error on ' + filename + '\nCaused by : \n' 
-          + e.message
-          + '\n\nStack : '
-          + e.stack
-        );
-      }
-    }
-    if ( code === false ) {
-      if (!ignore) {
-        util.error(
-          'Warning : T_INCLUDE error on ' + filename
-        );
-      }
-      return false;
-    } else {
-      return code.__main(
-        output ? output : process.stdout
-      );
-    }
-  }
-  // includes the specified script only one time
-  , include_once: function(filename, ignore, output) {
-    return this.include(filename, ignore, output, true);
-  }
-  // requires the specified script and throws an error if its not found
-  , require: function(filename, output, once) {
-    if (this.include(filename, true, output, once) === false) {
-      throw new Error(
-        "Error : Required file " + filename + " does not exists"
-      );
-    }
-  }
-  // require_once
-  , require_once: function(filename, output) {
-    return this.require(filename, output, true);
+  // configure the stdout object
+  if (this.config.hasOwnProperty('stdout')) {
+    this.writer = this.config.stdout;
   }
 };
-if (typeof(process.env.DEBUG) == 'undefined') process.env.DEBUG = 0;
-module.exports.clean();
+
+// define PHP the version
+Object.defineProperty(php.prototype, "version", {
+  enumerable: true,
+  configurable: false,
+  writable: false,
+  value: pkg.version
+});
+
+// define PHP output module
+Object.defineProperty(php.prototype, "stdout", {
+  enumerable: false,
+  configurable: false,
+  writable: true,
+  value: process.stdout.write
+});
+
+// define PHP output module
+Object.defineProperty(php.prototype, "stderr", {
+  enumerable: false,
+  configurable: false,
+  writable: true,
+  value: process.stderr.write
+});
+
+// defines the path module (resolves filenames)
+Object.defineProperty(php.prototype, "path", {
+  enumerable: false,
+  configurable: false,
+  writable: true,
+  value: require('path').resolve
+});
+
+// defines the path module (resolves filenames)
+Object.defineProperty(php.prototype, "context", {
+  enumerable: false,
+  configurable: false,
+  writable: true,
+  value: null
+});
+
+// helper for reading a constant
+Object.defineProperty(php.prototype, "constant", {
+  enumerable: false,
+  configurable: false,
+  writable: false,
+  value: function(key) {
+    return this.context.constants.get(name);
+  }
+});
+
+
+// defines the warning module
+Object.defineProperty(php.prototype, "trigger_error", {
+  enumerable: false,
+  configurable: false,
+  writable: false,
+  value: function(message, type, error) {
+    type = type || this.constant('E_USER_NOTICE');
+    var errType = 'Error';
+    switch(type) {
+      case this.constant('E_ERROR'): errType = 'Fatal Error'; break;
+      case this.constant('E_WARNING'): errType = 'Warning'; break;
+      case this.constant('E_PARSE'): errType = 'Parse Error'; break;
+      case this.constant('E_NOTICE'): errType = 'Notice'; break;
+      default:
+        return false;
+    }
+    // @todo : output with the standard error writer
+    this.stderr(
+      errType + ' : ' + message + '\nCaused by : \n' 
+      + e.message
+      + '\n\nStack : '
+      + e.stack
+    );
+    return true;
+  }
+});
+
+
+// define PHP execution context
+Object.defineProperty(php.prototype, "context", {
+  enumerable: false,
+  configurable: false,
+  writable: true,
+  value: null
+});
+
+/**
+ * run a eval over the specified PHP code
+ */
+php.prototype.eval = function(code) {
+  return this.context.eval(code);
+};
+
+/**
+ * run a eval over the specified PHP code
+ */
+php.prototype.include = function(filename) {
+  try {
+    return this.context.get(
+      this.path(filename)
+    );
+  } catch(e) {
+    this.trigger_error(
+      'T_INCLUDE error on ' + filename, this.constant('E_WARNING'), e
+    );
+    return false;
+  }
+};
+
+/**
+ * includes the specified script only one time
+ */
+php.prototype.include_once = function(filename) {
+  filename = this.path(filename);
+  if (!this.context.includes.hasOwnProperty(filename)) {
+    return this.include(filename);
+  }
+  return null;
+};
+
+/**
+ * requires the specified script and throws an error if its not found
+ */
+php.prototype.require = function(filename) {
+  var result = this.include(filename);
+  if (result === false) {
+    throw new Error(
+      "Error : Required file " + filename + " does not exists"
+    );
+  }
+  return result;
+};
+
+/**
+ * require_once
+ */
+php.prototype.require_once = function(filename) {
+  var result = this.include_once(filename);
+  if (result === false) {
+    throw new Error(
+      "Error : Required file " + filename + " does not exists"
+    );
+  }
+  return result;
+};
+
+// expose the php constructor
+module.exports = php;
