bc5497520c1351c9341f1039bec80566ec743f47
Ioan CHIRIAC <i.chiriac@yahoo.com>
2015-09-20 16:58:25 +0200

    proto

--- src/transcriptors/internal.js
+++ src/transcriptors/internal.js
@@ -5,7 +5,47 @@
  * @license BSD-3-Clause
  */
 
-module.exports = {
+module.exports = function(declare) {
+  declare('sys', {
+    echo: function(writer, ast) {
+      writer.write('php.stdout(');
+      for(var i = 0; i < ast[2].length; i++) {
+        writer.asString(ast[2][i]);
+      }
+      return writer.write(');\n');
+    }
+  });
+  declare('set', function(writer, ast) {
+    if (ast[1][0] === 'var') {
+      if (!writer.getContext().has(ast[1][1])) {
+        writer.getContext().set(
+          ast[1][1], writer.getCode(ast[2])
+        );
+      } else {
+        writer.write(ast[1][1] + ' = ');
+        writer.write(ast[2]);
+        writer.write(';');
+      }
+    } else {
+      throw new Error('???');
+    }
+  });
+  declare('bin', {
+    '.': function(writer, ast) {
+      writer.asString(ast[2]);
+      writer.write('+');
+      writer.asString(ast[3]);
+    }
+  });
+  declare('number', function(writer, ast) {
+    return writer.write(ast[1]);
+  });
+  declare('string', function(writer, ast) {
+    return writer.write('"' + ast[1] + '"');
+  });
+};
+
+/*
   init: function(builder) {
     return {
       T_ECHO: function(item) {
@@ -40,3 +80,4 @@ module.exports = {
     };
   }
 };
+*/
