adfaed8a2b01a031a48acef39c45d0b2afe2bd66
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-28 01:05:08 +0100

    remove old src folder

--- src/context/interfaces.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Glayzzle : PHP on NodeJS
- * @authors https://github.com/glayzzle/glayzzle/graphs/contributors
- * @license BSD-3-Clause
- * @url http://glayzzle.com
- */
-
-"use strict";
--- src/transcriptors/constants.js
+++
@@ -1,6 +0,0 @@
-/**
- * Glayzzle : PHP on NodeJS
- * @url http://glayzzle.com
- * @author Ioan CHIRIAC
- * @license BSD-3-Clause
- */
