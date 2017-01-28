bbe991d1f4d1b044bdc02bb43707fd5d3d9eedd7
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-28 11:58:33 +0100

    use handled nodes on fibo

--- test/fibo.php
+++ test/fibo.php
@@ -1,7 +1,11 @@
 <?php
-function fibo($a) {
-  return $a < 1 ? 1 : fibo($a - 2) + fibo($a - 1);
-}
-$start = microtime(true);
-echo ">>>" . fibo(30) . "\n";
-echo round(microtime(true) - $start, 3)."ms\n";
+  function fibo($a) {
+    if ($a < 2) {
+      return $a;
+    } else {
+      return fibo($a - 2) + fibo($a - 1);
+    }
+  }
+  $start = microtime(true);
+  echo ">>>" . fibo(40) . "\n";
+  echo round(microtime(true) - $start, 3)."ms\n";
